b0851a84fedd4f0f215ad3ac0d5433df08a3ab37
Bert Allen <mrlaughingdragon@aol.com>
2016-05-20 15:42:22 -0600

    hopefully still works

--- server-assets/api/api.js
+++ server-assets/api/api.js
@@ -10,29 +10,29 @@
     router.route('/sports/:id?')
         .get(Sport.getSports)
         .post(Sport.addSport)
-        .put(Sport.editSport)
-        .delete(Sport.removeSport);
+        // .put(Sport.editSport)
+        // .delete(Sport.removeSport);
         
     router.route('/leagues/:id?')
         .get(League.getLeagues)
         .post(League.addLeague)
-        .put(League.editLeague)
-        .delete(League.removeLeague);
+        // .put(League.editLeague)
+        // .delete(League.removeLeague);
     /**
      * Your Code Here
      */
 
-    router.route('/teams/:id?')
-        .get(Team.getTeams)
-        .post(Team.addTeam)
-        .put(Team.editTeam)
-        .delete(Team.removeTeam);
+    // router.route('/teams/:id?')
+    //     .get(Team.getTeams)
+    //     .post(Team.addTeam)
+    //     .put(Team.editTeam)
+    //     .delete(Team.removeTeam);
 
-    router.route('/players/:id?')
-        .get(Player.getPlayers)
-        .post(Player.addPlayer)
-        .put(Player.editPlayer)
-        .delete(Player.removePlayer);    
+    // router.route('/players/:id?')
+    //     .get(Player.getPlayers)
+    //     .post(Player.addPlayer)
+    //     .put(Player.editPlayer)
+    //     .delete(Player.removePlayer);    
     
 	//Make sure you export the router component
 	exports.router = router;
